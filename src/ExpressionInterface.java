f0a9c7aed311a5bf695e6a0181fce6f1174e3f59
Ersin Arioglu <ersin@mit.edu>
2019-01-10 11:41:03 -0500

    Added new Expression types and wrote interface for expression type.

--- src/ExpressionInterface.java
+++ src/ExpressionInterface.java
@@ -1,3 +1,31 @@
 package formulaic;
 
 
+/**
+ * The immutable type that represents an algebraic formula.
+ */
+
+public interface Expression {
+    /*
+     * Datatype definition:
+     * Expression = TODO
+     * 
+     */
+    
+    
+    /**
+     * Returns a simplified version of the formula given.
+     * @param exp Expression object to be simplified.
+     * @return Expression representing the simplified version of the formula.
+     */
+    public Expression simplify(Expression exp);
+    
+    
+    /**
+     * Returns the derivative of the formula given.
+     * @param exp Expression object to be derived.
+     * @return Expression representing the simplified version of the formula.
+     */
+    public Expression derivative(Expression exp);
+    
+}
